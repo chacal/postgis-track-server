ef0dfc2536dabae8ef109ac16f7f9b123e2f040c
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2017-06-11 22:52:58 +0300

    Use 'postgres' as default DB user

--- src/Db.ts
+++ src/Db.ts
@@ -7,7 +7,7 @@ const dbConfig = {
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || 5432,
   database: process.env.DB_NAME || 'signalk',
-  user: process.env.DB_USER || 'jihartik',
+  user: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWD
 }
 
